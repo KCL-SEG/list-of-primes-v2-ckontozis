8cb2d1c77936c4fd72fda3d936b6a5023bec618d
Constantinos Kontozis <constantinos.kontozis@kcl.ac.uk>
2023-10-01 00:56:21 +0100

    Add ValueError raised when input is not a positive number

--- primes.py
+++ primes.py
@@ -1,6 +1,16 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
     list = []
+    num = 2
+    if (number_of_primes > 0):
+        while len(list) < number_of_primes:
+            if all(num % prime != 0 for prime in list):
+                list.append(num)
+            num += 1
+    else:
+        raise ValueError(
+            f"You entered {number_of_primes}, which is not a positive number.")
     return list
